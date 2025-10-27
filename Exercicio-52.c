191266a1e00983f8e14db706f3fcbf34312ba6f2
OnoHome <0000934360@senaimgaluno.com.br>
2025-10-27 00:19:11 -0300

    Progresso dnv

--- Exercicio-52.c
+++ Exercicio-52.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    int idade, i, soma, maioridade, maior18, abaixocinco;
-    media = soma / 10;
+    int idade, i = 1, soma = 0, maioridade = 0, maior18 = 0, abaixocinco = 0, media = 0;
     while (i <= 10)
     {
         printf("Insira a sua idade: ");
@@ -20,9 +19,12 @@ int main()
         {
             maioridade = idade;
         }
-
         i++;
     }
-
+    media = soma / 10;
+    printf("A média de idade do grupo é: %d\n", media);
+    printf("Pessoas com mais de 18 anos: %d\n", maior18);
+    printf("No grupo %d Pessoas tem menos de 5 anos \n", abaixocinco);
+    printf("A maior idade apresentada é: %d\n", maioridade);
     return 0;
 }
